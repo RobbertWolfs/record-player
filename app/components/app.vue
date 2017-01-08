4912f066644948335b2a97f8d0dcb84c45a953f9
Robbert Wolfs <robbertwolfs@gmail.com>
2017-01-08 14:18:35 +0100

    Day 5: Did some more styling of my vinyl vue component

--- app/components/app.vue
+++ app/components/app.vue
@@ -2,7 +2,7 @@
     div
         h1.title {{ title }}
 
-        vinyl
+        vinyl(v-for="vinyl in vinyls" v-bind:artist="vinyl.artist" v-bind:album="vinyl.album" v-bind:visual="vinyl.visual")
 </template>
 
 <script>
@@ -12,6 +12,38 @@
         data() {
             return {
                 title: '#100daysOfCode Record Player',
+                vinyls: [
+                    {
+                        artist: 'Daft Punk',
+                        album: 'Random Access Memories',
+                        visual: 'https://images-na.ssl-images-amazon.com/images/I/61Ia07wdZQL._SL1500_.jpg',
+                    },
+                    {
+                        artist: 'Colplay',
+                        album: 'Parachutes',
+                        visual: 'http://assets2.thecreatorsproject.com/content-images/contentimage/no-slug/32b467be1ad9b7c6db2671bc3ffd69ab.jpg',
+                    },
+                    {
+                        artist: 'The Mars Volta',
+                        album: 'Amputechture',
+                        visual: 'http://vinylcoverart.com/pics/a/the-mars-volta-amputechture.jpg',
+                    },
+                    {
+                        artist: 'Capital Cities',
+                        album: 'In A Tidal Wave Of Mystery',
+                        visual: 'http://vinylcoverart.com/pics/i/capital-cities-in-a-tidal-wave-of-mystery-tn.jpg',
+                    },
+                    {
+                        artist: 'Bill Frisell',
+                        album: 'Guitar In The Space Age!',
+                        visual: 'http://vinylcoverart.com/pics/g/bill-frisell-guitar-in-the-space-age-tn.jpg',
+                    },
+                    {
+                        artist: 'Pink Floyd',
+                        album: 'The Dark Side of the Moon',
+                        visual: 'http://theludlowgroup.com/associated-files/posts/2014-05-06-the-lost-art-of-album-covers/Vinyl_Album_Cover_Storm_Thorgerson_Pink_Floyd_Dark_Side_Of_The_Moon_2.jpg',
+                    },
+                ],
             }
         },
         components: { Vinyl },
--- app/components/vinyl.vue
+++ app/components/vinyl.vue
@@ -1,18 +1,17 @@
 <template lang="pug">
-    .vinyl
-        h1.name {{ name }}
+    .vinyl(v-if="visual")
         .vinyl__visual 
             img(:src="visual")
+            .record  
+        .vinyl__info(v-if="artist")
+            h3.info__artist {{ artist }}
+            p.info__album {{ album }}
+        
 </template>
 
 <script>
     export default {
-        data () {
-            return {
-                name: 'Daft Punk',
-                visual: 'https://images-na.ssl-images-amazon.com/images/I/61Ia07wdZQL._SL1500_.jpg',
-            }
-        }
+        props: ['visual', 'artist', 'album'],
     }
 </script>
 
@@ -22,23 +21,79 @@
 
     $height: 300px;
     $width: 300px;
+    $visibleRecordSize: 30px;
 
     .vinyl {
-        border: 2px $secondaryColor solid;
-        height: $height;
+        display: inline-block;
         position: relative;
-        width: $width;
+        margin: 10px;
 
-        .name {
-            border: 5px $secondaryColor solid;
-            padding: 10px;
-            position: absolute;
-            right: calc(-#{$width} / 4);
-            bottom: calc(-#{$height} / 4);
+        &:hover {
+            cursor: pointer;
+            z-index: 3;
+        
+            .record {
+                box-shadow: 10px 0 50px 1px rgba(0,0,0,.3);
+                left: $visibleRecordSize * 1.5;   
+                -webkit-box-shadow: 10px 0 50px 1px rgba(255,255,255,.1);
+            }
+        }
+    
+        &__visual {
+            height: $height;
+            position: relative;
+            width: $width + $visibleRecordSize;
+
+            img {
+                border: 3px $secondaryColor solid;
+            }
+
+            .record {
+                background-color: black;
+                border-radius: $width / 2;
+                display: inline-block;
+                height: $height;
+                position: absolute;
+                top: 0;
+                left: $visibleRecordSize;
+                width: $width;
+                z-index: -1;
+
+                // transition
+                -webkit-transition: left 100ms ease-out;
+                -moz-transition: left 100ms ease-out;
+                -ms-transition: left 100ms ease-out;
+                -o-transition: left 100ms ease-out;
+                transition: left 100ms ease-out;
+            }
         }
 
-        &__visual {
+        &__info {
+            margin-top: -10px;
+            left: 15px;
+            position: relative;
 
+            .info {
+                &__artist,
+                &__album {
+                    position: relative;
+                    margin: 0;
+                }
+               
+                &__artist {
+                    background-color: $secondaryColor;
+                    border: 2px $primaryColor solid;
+                    color: $primaryColor;
+                    display: inline-block;
+                    padding: 5px 10px;
+                }
+
+                &__album {
+                    margin-left: 20px;
+                    margin-top: 5px;
+                }
+            }
         }
     }
 </style>
+
